b77d630ce3fc9e6754b5b5d920c382eeedcf4db3
Michael Winsor <github@tape-worm.net>
2021-02-12 00:46:11 -0700

    Replaced SharpDX math with System.Numerics (again).

--- BCnEnc.Net/Shared/PcaVectors.cs
+++ BCnEnc.Net/Shared/PcaVectors.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using DX = SharpDX;
+using System.Numerics;
 using Gorgon.Graphics;
 
 namespace BCnEncoder.Shared
@@ -9,7 +9,7 @@ namespace BCnEncoder.Shared
 		private const int C565_5_mask = 0xF8;
 		private const int C565_6_mask = 0xFC;
 
-		private static void ConvertToVector4(ReadOnlySpan<GorgonColor> colors, Span<DX.Vector4> vectors)
+		private static void ConvertToVector4(ReadOnlySpan<GorgonColor> colors, Span<Vector4> vectors)
 		{
 			for (int i = 0; i < colors.Length; i++)
 			{
@@ -21,7 +21,7 @@ namespace BCnEncoder.Shared
 			}
 		}
 
-		private static void CalculateMean(Span<DX.Vector4> colors, out DX.Vector4 result)
+		private static void CalculateMean(Span<Vector4> colors, out Vector4 result)
 		{
 
 			float r = 0;
@@ -37,7 +37,7 @@ namespace BCnEncoder.Shared
 				a += colors[i].W;
 			}
 
-			result = new DX.Vector4(
+			result = new Vector4(
 				r / colors.Length,
 				g / colors.Length,
 				b / colors.Length,
@@ -45,7 +45,7 @@ namespace BCnEncoder.Shared
 				);
 		}
 
-		internal static void CalculateCovariance(Span<DX.Vector4> values, out DX.Vector4 mean, out DX.Matrix result) {
+		internal static void CalculateCovariance(Span<Vector4> values, out Vector4 mean, out Matrix4x4 result) {
 			CalculateMean(values, out mean);
 			for (int i = 0; i < values.Length; i++)
 			{
@@ -72,7 +72,7 @@ namespace BCnEncoder.Shared
 				result.M44 += values[i].W * values[i].W;
 			}
 
-			DX.Matrix.Multiply(ref result, 1f / (values.Length - 1), out result);
+			result = Matrix4x4.Multiply(result, 1f / (values.Length - 1));
 			
 			result.M21 = result.M12;
 			result.M31 = result.M13;
@@ -88,18 +88,18 @@ namespace BCnEncoder.Shared
 		/// <param name="covarianceMatrix"></param>
 		/// <param name="result"></param>
 		/// <returns></returns>
-		internal static void CalculatePrincipalAxis(ref DX.Matrix covarianceMatrix, out DX.Vector4 result) {
-			result = DX.Vector4.UnitY;
+		internal static void CalculatePrincipalAxis(ref Matrix4x4 covarianceMatrix, out Vector4 result) {
+			result = Vector4.UnitY;
 
 			for (int i = 0; i < 30; i++) {
-				DX.Vector4.Transform(ref result, ref covarianceMatrix, out DX.Vector4 dA);
+				var dA = Vector4.Transform(result, covarianceMatrix);
 
 				if(dA.LengthSquared() == 0) {
 					break;
 				}
 
-				dA = DX.Vector4.Normalize(dA);
-				DX.Vector4.Dot(ref result, ref dA, out float dot);
+				dA = Vector4.Normalize(dA);
+				float dot = Vector4.Dot(result, dA);
 				if (dot > 0.999999) {
 					result = dA;
 					break;
@@ -110,37 +110,37 @@ namespace BCnEncoder.Shared
 			}
 		}
 
-		public static void Create(Span<GorgonColor> colors, out DX.Vector3 mean, out DX.Vector3 principalAxis)
+		public static void Create(Span<GorgonColor> colors, out Vector3 mean, out Vector3 principalAxis)
 		{
-			Span<DX.Vector4> vectors = stackalloc DX.Vector4[colors.Length];
+			Span<Vector4> vectors = stackalloc Vector4[colors.Length];
 			ConvertToVector4(colors, vectors);
 
 
-            CalculateCovariance(vectors, out DX.Vector4 v4Mean, out DX.Matrix cov);
-			mean = new DX.Vector3(v4Mean.X, v4Mean.Y, v4Mean.Z);
+            CalculateCovariance(vectors, out Vector4 v4Mean, out Matrix4x4 cov);
+			mean = new Vector3(v4Mean.X, v4Mean.Y, v4Mean.Z);
 
-            CalculatePrincipalAxis(ref cov, out DX.Vector4 pa);
-			principalAxis = new DX.Vector3(pa.X, pa.Y, pa.Z);
+            CalculatePrincipalAxis(ref cov, out Vector4 pa);
+			principalAxis = new Vector3(pa.X, pa.Y, pa.Z);
 			if (principalAxis.LengthSquared() == 0) {
-				principalAxis = DX.Vector3.UnitY;
+				principalAxis = Vector3.UnitY;
 			}
 			else {
-				principalAxis = DX.Vector3.Normalize(principalAxis);
+				principalAxis = Vector3.Normalize(principalAxis);
 			}
 
 		}
 
-		public static void CreateWithAlpha(Span<GorgonColor> colors, out DX.Vector4 mean, out DX.Vector4 principalAxis)
+		public static void CreateWithAlpha(Span<GorgonColor> colors, out Vector4 mean, out Vector4 principalAxis)
 		{
-			Span<DX.Vector4> vectors = stackalloc DX.Vector4[colors.Length];
+			Span<Vector4> vectors = stackalloc Vector4[colors.Length];
 			ConvertToVector4(colors, vectors);
 
-            CalculateCovariance(vectors, out mean, out DX.Matrix cov);
+            CalculateCovariance(vectors, out mean, out Matrix4x4 cov);
 			CalculatePrincipalAxis(ref cov, out principalAxis);
 		}
 
 
-		public static void GetExtremePoints(Span<int> colors, DX.Vector3 mean, DX.Vector3 principalAxis, out ColorRgb24 min, out ColorRgb24 max)
+		public static void GetExtremePoints(Span<int> colors, Vector3 mean, Vector3 principalAxis, out ColorRgb24 min, out ColorRgb24 max)
 		{
 
 			float minD = 0;
@@ -150,8 +150,8 @@ namespace BCnEncoder.Shared
 			{
 				var colorVec = GorgonColor.FromRGBA(colors[i]).ToVector3();
 
-				DX.Vector3.Subtract(ref colorVec, ref mean, out DX.Vector3 v);
-                float d = DX.Vector3.Dot(v, principalAxis);
+				Vector3 v = Vector3.Subtract(colorVec, mean);
+                float d = Vector3.Dot(v, principalAxis);
 				if (d < minD)
                 {
                     minD = d;
@@ -163,10 +163,10 @@ namespace BCnEncoder.Shared
                 }
             }
 
-			DX.Vector3.Multiply(ref principalAxis, minD, out DX.Vector3 minAxis);
-			DX.Vector3.Multiply(ref principalAxis, maxD, out DX.Vector3 maxAxis);
-			DX.Vector3.Add(ref mean, ref minAxis, out DX.Vector3 minVec);
-			DX.Vector3.Add(ref mean, ref maxAxis, out DX.Vector3 maxVec);
+			var minAxis = Vector3.Multiply(principalAxis, minD);
+			var maxAxis = Vector3.Multiply(principalAxis, maxD);
+			var minVec = Vector3.Add(mean, minAxis);
+			var maxVec = Vector3.Add(mean, maxAxis);
 
 			int minR = (int) (minVec.X * 255);
 			int minG = (int) (minVec.Y * 255);
@@ -188,7 +188,7 @@ namespace BCnEncoder.Shared
 			max = new ColorRgb24((byte)maxR, (byte)maxG, (byte)maxB);
 		}
 
-		public static void GetMinMaxColor565(Span<GorgonColor> colors, DX.Vector3 mean, DX.Vector3 principalAxis, 
+		public static void GetMinMaxColor565(Span<GorgonColor> colors, Vector3 mean, Vector3 principalAxis, 
 			out ColorRgb565 min, out ColorRgb565 max)
 		{
 
@@ -197,10 +197,10 @@ namespace BCnEncoder.Shared
 
 			for (int i = 0; i < colors.Length; i++)
 			{
-				var colorVec = (DX.Vector3)colors[i];
+				var colorVec = colors[i].ToVector3();
 
-				DX.Vector3.Subtract(ref colorVec, ref mean, out DX.Vector3 v);
-				DX.Vector3.Dot(ref v, ref principalAxis, out float d);
+				Vector3 v = Vector3.Subtract(colorVec, mean);
+				float d = Vector3.Dot(v, principalAxis);
 				if (d < minD)
                 {
                     minD = d;
@@ -216,10 +216,10 @@ namespace BCnEncoder.Shared
 			minD *= 15 / 16f;
 			maxD *= 15 / 16f;
 
-			DX.Vector3.Multiply(ref principalAxis, minD, out DX.Vector3 minAxis);
-			DX.Vector3.Multiply(ref principalAxis, maxD, out DX.Vector3 maxAxis);
-			DX.Vector3.Add(ref mean, ref minAxis, out DX.Vector3 minVec);
-			DX.Vector3.Add(ref mean, ref maxAxis, out DX.Vector3 maxVec);
+			var minAxis = Vector3.Multiply(principalAxis, minD);
+			var maxAxis = Vector3.Multiply(principalAxis, maxD);
+			var minVec = Vector3.Add(mean, minAxis);
+			var maxVec = Vector3.Add(mean, maxAxis);
 
 			int minR = (int) (minVec.X * 255);
 			int minG = (int) (minVec.Y * 255);
@@ -251,7 +251,7 @@ namespace BCnEncoder.Shared
 
 		}
 
-		public static void GetExtremePointsWithAlpha(Span<GorgonColor> colors, DX.Vector4 mean, DX.Vector4 principalAxis, out DX.Vector4 min, out DX.Vector4 max)
+		public static void GetExtremePointsWithAlpha(Span<GorgonColor> colors, Vector4 mean, Vector4 principalAxis, out Vector4 min, out Vector4 max)
 		{
 
 			float minD = 0;
@@ -259,9 +259,9 @@ namespace BCnEncoder.Shared
 
 			for (int i = 0; i < colors.Length; i++)
 			{				
-				DX.Vector4 colorVec = colors[i];
-				DX.Vector4.Subtract(ref colorVec, ref mean, out DX.Vector4 v);
-				DX.Vector4.Dot(ref v, ref principalAxis, out float d);
+				Vector4 colorVec = colors[i].ToVector4();
+				var v = Vector4.Subtract(colorVec, mean);
+				float d = Vector4.Dot(v, principalAxis);
 				if (d < minD)
                 {
                     minD = d;
@@ -273,17 +273,17 @@ namespace BCnEncoder.Shared
                 }
             }
 
-			DX.Vector4.Multiply(ref principalAxis, minD, out DX.Vector4 axisMin);
-			DX.Vector4.Multiply(ref principalAxis, maxD, out DX.Vector4 axisMax);
-			DX.Vector4.Add(ref mean, ref axisMin, out min);
-			DX.Vector4.Add(ref mean, ref axisMax, out max);
+			var minAxis = Vector4.Multiply(principalAxis, minD);
+			var maxAxis = Vector4.Multiply(principalAxis, maxD);
+			min = Vector4.Add(mean, minAxis);
+			max = Vector4.Add(mean, maxAxis);
 		}
 
-		public static void GetOptimizedEndpoints565(Span<int> colors, DX.Vector3 mean, DX.Vector3 principalAxis, out ColorRgb565 min, out ColorRgb565 max,
+		public static void GetOptimizedEndpoints565(Span<int> colors, Vector3 mean, Vector3 principalAxis, out ColorRgb565 min, out ColorRgb565 max,
 			float rWeight = 0.3f, float gWeight = 0.6f, float bWeight = 0.1f)
 		{
 			int length = colors.Length;
-			var vectorColors = new DX.Vector3[length];
+			var vectorColors = new Vector3[length];
 			for (int i = 0; i < colors.Length; i++)
 			{
 				vectorColors[i] = GorgonColor.FromRGBA(colors[i]).ToVector3();
@@ -292,7 +292,7 @@ namespace BCnEncoder.Shared
 			float minD = 0;
 			float maxD = 0;
 
-			void Clamp565(ref DX.Vector3 vec)
+			void Clamp565(ref Vector3 vec)
 			{
 				if (vec.X < 0)
                 {
@@ -324,15 +324,15 @@ namespace BCnEncoder.Shared
                     vec.Z = 31;
                 }
 
-                vec = new DX.Vector3((float)Math.Round(vec.X), (float)Math.Round(vec.Y), (float)Math.Round(vec.Z));
+                vec = new Vector3((float)Math.Round(vec.X), (float)Math.Round(vec.Y), (float)Math.Round(vec.Z));
 			}
 
-            void Distance(ref DX.Vector3 v, ref DX.Vector3 p, out float dist) 
+            void Distance(ref Vector3 v, ref Vector3 p, out float dist) 
 				=> dist = (v.X - p.X) * (v.X - p.X) * rWeight
                        + (v.Y - p.Y) * (v.Y - p.Y) * gWeight
                        + (v.Z - p.Z) * (v.Z - p.Z) * bWeight;
 
-            float SelectClosestDistance(ref DX.Vector3 selector, ref DX.Vector3 f0, ref DX.Vector3 f1, ref DX.Vector3 f2, ref DX.Vector3 f3)
+            float SelectClosestDistance(ref Vector3 selector, ref Vector3 f0, ref Vector3 f1, ref Vector3 f2, ref Vector3 f3)
 			{
 				Distance(ref selector, ref f0, out float d0);
 				Distance(ref selector, ref f1, out float d1);
@@ -342,20 +342,20 @@ namespace BCnEncoder.Shared
                 return d0 < d1 && d0 < d2 && d0 < d3 ? d0 : d1 < d0 && d1 < d2 && d1 < d3 ? d1 : d2 < d0 && d2 < d1 && d2 < d3 ? d2 : d3;
             }
 
-            DX.Vector3 endPoint0;
-			DX.Vector3 endPoint1;
+            Vector3 endPoint0;
+			Vector3 endPoint1;
 
 			double CalculateError()
 			{
 				double cumulativeError = 0;
-				var ep0 = new DX.Vector3(endPoint0.X / 31, endPoint0.Y / 63, endPoint0.Z / 31);
-				var ep1 = new DX.Vector3(endPoint1.X / 31, endPoint1.Y / 63, endPoint1.Z / 31);
+				var ep0 = new Vector3(endPoint0.X / 31, endPoint0.Y / 63, endPoint0.Z / 31);
+				var ep1 = new Vector3(endPoint1.X / 31, endPoint1.Y / 63, endPoint1.Z / 31);
 
-				DX.Vector3.Subtract(ref ep1, ref ep0, out DX.Vector3 diff);
-				DX.Vector3.Divide(ref diff, 3.0f, out DX.Vector3 ep3rds);
-				DX.Vector3.Divide(ref diff, 2.0f / 3.0f, out DX.Vector3 ep2_3rds);
-				DX.Vector3.Add(ref ep0, ref ep3rds, out DX.Vector3 ep2);
-				DX.Vector3.Add(ref ep0, ref ep2_3rds, out DX.Vector3 ep3);
+				var diff = Vector3.Subtract(ep1, ep0);
+				var ep3rds = Vector3.Divide(diff, 3.0f);
+				var ep2_3rds = Vector3.Divide(diff, 2.0f / 3.0f);
+				var ep2 = Vector3.Add(ep0, ep3rds);
+				var ep3 = Vector3.Add(ep0, ep2_3rds);
 
 				for (int i = 0; i < length; i++)
 				{
@@ -380,13 +380,13 @@ namespace BCnEncoder.Shared
                 }
             }
 
-			DX.Vector3.Multiply(ref principalAxis, minD, out DX.Vector3 minAxis);
-			DX.Vector3.Multiply(ref principalAxis, maxD, out DX.Vector3 maxAxis);
-			DX.Vector3.Add(ref mean, ref minAxis, out endPoint0);
-			DX.Vector3.Add(ref mean, ref maxAxis, out endPoint1);
+			var minAxis = Vector3.Multiply(principalAxis, minD);
+			var maxAxis = Vector3.Multiply(principalAxis, maxD);
+			endPoint0 = Vector3.Add(mean, minAxis);
+			endPoint1 = Vector3.Add(mean, maxAxis);
 
-			endPoint0 = new DX.Vector3((float)Math.Round(endPoint0.X * 31), (float)Math.Round(endPoint0.Y * 63), (float)Math.Round(endPoint0.Z * 31));
-			endPoint1 = new DX.Vector3((float)Math.Round(endPoint1.X * 31), (float)Math.Round(endPoint1.Y * 63), (float)Math.Round(endPoint1.Z * 31));
+			endPoint0 = new Vector3((float)Math.Round(endPoint0.X * 31), (float)Math.Round(endPoint0.Y * 63), (float)Math.Round(endPoint0.Z * 31));
+			endPoint1 = new Vector3((float)Math.Round(endPoint1.X * 31), (float)Math.Round(endPoint1.Y * 63), (float)Math.Round(endPoint1.Z * 31));
 			Clamp565(ref endPoint0);
 			Clamp565(ref endPoint1);
 
@@ -400,10 +400,10 @@ namespace BCnEncoder.Shared
 				rounds++;
 				foundBetter = false;
 				{ // decrement ep0
-                    DX.Vector3 prev = endPoint0;
+                    Vector3 prev = endPoint0;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Subtract(ref endPoint0, ref axisP, out endPoint0);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint0 = Vector3.Subtract(endPoint0, axisP);
 					Clamp565(ref endPoint0);
 					double error = CalculateError();
 					if (error < best)
@@ -418,10 +418,10 @@ namespace BCnEncoder.Shared
 				}
 
 				{ // decrement ep1
-                    DX.Vector3 prev = endPoint1;
+                    Vector3 prev = endPoint1;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Subtract(ref endPoint1, ref axisP, out endPoint1);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint1 = Vector3.Subtract(endPoint1, axisP);
 
 					Clamp565(ref endPoint1);
 					double error = CalculateError();
@@ -437,10 +437,10 @@ namespace BCnEncoder.Shared
 				}
 
 				{ // increment ep0
-                    DX.Vector3 prev = endPoint0;
+                    Vector3 prev = endPoint0;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Add(ref endPoint0, ref axisP, out endPoint0);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint0 = Vector3.Add(endPoint0, axisP);
 
 					Clamp565(ref endPoint0);
 					double error = CalculateError();
@@ -456,10 +456,10 @@ namespace BCnEncoder.Shared
 				}
 
 				{ // increment ep1
-                    DX.Vector3 prev = endPoint1;
+                    Vector3 prev = endPoint1;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Add(ref endPoint1, ref axisP, out endPoint1);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint1 = Vector3.Add(endPoint1, axisP);
 
 					Clamp565(ref endPoint1);
 					double error = CalculateError();
@@ -475,12 +475,12 @@ namespace BCnEncoder.Shared
 				}
 
 				{ // scaleUp 
-                    DX.Vector3 prev0 = endPoint0;
-                    DX.Vector3 prev1 = endPoint1;
+                    Vector3 prev0 = endPoint0;
+                    Vector3 prev1 = endPoint1;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Subtract(ref endPoint0, ref axisP, out endPoint0);
-					DX.Vector3.Add(ref endPoint1, ref axisP, out endPoint0);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint0 = Vector3.Subtract(endPoint0, axisP);
+					endPoint0 = Vector3.Add(endPoint1, axisP);
 
 					Clamp565(ref endPoint0);
 					Clamp565(ref endPoint1);
@@ -498,12 +498,12 @@ namespace BCnEncoder.Shared
 				}
 
 				{ // scaleDown
-                    DX.Vector3 prev0 = endPoint0;
-                    DX.Vector3 prev1 = endPoint1;
+                    Vector3 prev0 = endPoint0;
+                    Vector3 prev1 = endPoint1;
 
-					DX.Vector3.Multiply(ref principalAxis, increment * 2, out DX.Vector3 axisP);
-					DX.Vector3.Add(ref endPoint0, ref axisP, out endPoint0);
-					DX.Vector3.Subtract(ref endPoint1, ref axisP, out endPoint0);
+					var axisP = Vector3.Multiply(principalAxis, increment * 2);
+					endPoint0 = Vector3.Add(endPoint0, axisP);
+					endPoint0 = Vector3.Subtract(endPoint1, axisP);
 
 					Clamp565(ref endPoint0);
 					Clamp565(ref endPoint1);
@@ -760,10 +760,10 @@ namespace BCnEncoder.Shared
             };
         }
 
-		private static void ProjectPointOnLine(ref DX.Vector3 point, ref DX.Vector3 linePoint, ref DX.Vector3 lineDir, out float d)
+		private static void ProjectPointOnLine(ref Vector3 point, ref Vector3 linePoint, ref Vector3 lineDir, out float d)
 		{
-			DX.Vector3.Subtract(ref point, ref linePoint, out DX.Vector3 v);
-			DX.Vector3.Dot(ref v, ref lineDir, out d);            
+			var v = Vector3.Subtract(point, linePoint);
+			d = Vector3.Dot(v, lineDir);            
 		}
 	}
 }
